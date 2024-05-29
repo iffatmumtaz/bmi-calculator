6232294bba930fecb22cf6591a2c25364aa2c068
Iffat mumtaz <160539573+iffatmumtaz@users.noreply.github.com>
2024-05-29 11:49:25 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+var weightInKg = 45;
+var heightInMeters = 1.5;
+var bmi = weightInKg / (heightInMeters * weightInKg);
+console.log("your BMI is ".concat(bmi));
---
+++ index.ts
@@ -0,0 +1,4 @@
+let weightInKg=45;
+let heightInMeters=1.5
+let bmi=weightInKg/(heightInMeters*weightInKg);
+console.log(`your BMI is ${bmi}`)
